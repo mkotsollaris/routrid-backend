1dc7cf3dfc154df2bc4b87a640774b00a3dea3ec
MishaBindra <mishasapra@yahoo.com>
2019-09-25 14:10:33 +0100

    WIP json parsing

---
+++ src/main/java/com/bankapp/business/model/BusinessDataDAO.java
@@ -0,0 +1,12 @@
+package com.bankapp.business.model;
+
+public class BusinessDataDAO {
+
+    private BusinessDataRequestDAO body;
+
+    public BusinessDataDAO(BusinessDataRequestDAO body){
+        this.body = body;
+    }
+
+
+}
